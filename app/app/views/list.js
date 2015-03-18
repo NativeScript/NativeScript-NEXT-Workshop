fe6b33da6fdf26e653cdf21672cb13eb8dc49aa1
TJ VanToll <tj.vantoll@gmail.com>
2015-03-18 12:56:42 -0400

    Add a navbar for ios

--- app/app/views/list.js
+++ app/app/views/list.js
@@ -1,3 +1,5 @@
+var applicationModule = require("application");
+var frameModule = require("ui/frame");
 var observableModule = require("data/observable");
 var observableArray = require("data/observable-array");
 
@@ -8,4 +10,15 @@ data.set("memes", memes);
 exports.load = function(args) {
 	var page = args.object;
 	page.bindingContext = data;
+
+	// Make sure we're on iOS before configuring the navigation bar
+	if (applicationModule.ios) {
+		page.ios.title = "MEME Generator";
+
+		// Get access to the native iOS UINavigationController
+		var controller = frameModule.topmost().ios.controller;
+
+		// Call the UINavigationController's setNavigationBarHidden method
+		controller.navigationBarHidden = false;
+	}
 };
