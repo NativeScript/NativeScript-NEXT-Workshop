406fe72bcd751290766eb8f3c74702a61010617e
TJ VanToll <tj.vantoll@gmail.com>
2015-03-31 09:35:03 -0400

    Getting closer :)

--- app/app/components/image-manipulation/image-manipulation.ios.js
+++ app/app/components/image-manipulation/image-manipulation.ios.js
@@ -1,6 +1,8 @@
 var imageSource = require("image-source");
 var templates = require("../templates/templates");
 
+var font = UIFont.boldSystemFontOfSize(12);
+
 module.exports = {
 	addText: function(templateIndex, topText, bottomText) {
 		topText = "One does not simply";
@@ -9,16 +11,22 @@ module.exports = {
 		// Get the UIImage
 		var image = templates.getByIndex(templateIndex).source.ios;
 
-		var font = UIFont.boldSystemFontOfSize(12);
 		UIGraphicsBeginImageContext(image.size);
+		image.drawInRect(
+			CGRectMake(0, 0, image.size.width, image.size.height)
+		);
 
-		var rect = CGRectMake(0, 0, image.size.width, image.size.height);
+		var topRect = CGRectMake(0, 0, image.size.width, image.size.height);
+		var bottomRect = CGRectMake(100, 100, image.size.width, image.size.height);
 
 		UIColor.whiteColor().set();
 
 		var attrDict = NSMutableDictionary.alloc().init();
-		var nsAttributedString = NSMutableAttributedString.alloc().initWithStringAttributes(topText, attrDict);
-		nsAttributedString.drawInRect( rect );
+		var topString = NSMutableAttributedString.alloc().initWithStringAttributes(topText, attrDict);
+		topString.drawInRect( topRect );
+
+		var bottomString = NSMutableAttributedString.alloc().initWithStringAttributes(bottomText, attrDict);
+		bottomString.drawInRect( topRect );
 
 		var newImage = UIGraphicsGetImageFromCurrentImageContext();
 		UIGraphicsEndImageContext();
