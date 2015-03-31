99c1ab38d148eedd47d11f420c4336e3b030783c
TJ VanToll <tj.vantoll@gmail.com>
2015-03-31 15:33:38 -0400

    Adding the basics of iOS sharing

--- app/app/components/social-share/social-share.ios.js
+++ app/app/components/social-share/social-share.ios.js
@@ -1,5 +1,8 @@
 module.exports = {
 	share: function(image) {
-
+		var activityController = UIActivityViewController.alloc()
+			.initWithActivityItemsApplicationActivities([], []);
+		UIApplication.sharedApplication().keyWindow.rootViewController
+			.presentViewControllerAnimatedCompletion(activityController, true, null);
 	}
 };
