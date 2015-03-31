2ed948d52c648a8f222aac53b541a147f7bb7dde
TJ VanToll <tj.vantoll@gmail.com>
2015-03-31 17:32:30 -0400

    Getting basic text sharing working on Android. Need to get image sharing working next

--- app/app/components/social-share/social-share.android.js
+++ app/app/components/social-share/social-share.android.js
@@ -1,5 +1,15 @@
+var application = require("application");
+
 module.exports = {
 	share: function(image) {
+		var message = "Text I want to share."
+		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(android.content.Intent.EXTRA_TEXT, message);
 
+		var context = application.android.context;
+		var shareIntent = android.content.Intent.createChooser(intent, "How would you like to share this image?");
+		shareIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(shareIntent);
 	}
 };
