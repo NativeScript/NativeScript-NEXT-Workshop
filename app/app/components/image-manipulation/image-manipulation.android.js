0e93923b27370eaf223ada1f3220e4ae1f7c5a42
TJ VanToll <tj.vantoll@gmail.com>
2015-03-31 12:46:25 -0400

    Meme generation now works on Android

--- app/app/components/image-manipulation/image-manipulation.android.js
+++ app/app/components/image-manipulation/image-manipulation.android.js
@@ -1,9 +1,28 @@
+var application = require("application");
+var imageSource = require("image-source");
+
 module.exports = {
 	addText: function(image, topText, bottomText) {
 		topText = topText || "";
 		bottomText = bottomText || "";
 
-		// Get the Bitmap instance
-		image = image.android;
+		// Android makes you create a mutable Bitmap from an immutable one. Because Java.
+		var bitmap = image.android.copy(android.graphics.Bitmap.Config.ARGB_8888, true);
+
+		var type = android.graphics.Typeface.create("Helvetica",
+			android.graphics.Typeface.BOLD);
+
+		var paint = new android.graphics.Paint();
+		paint.setStyle(android.graphics.Paint.Style.FILL);
+		paint.setColor(android.graphics.Color.WHITE);
+		paint.setTypeface(type);
+		paint.setTextAlign(android.graphics.Paint.Align.LEFT);
+		paint.setTextSize(40);
+
+		var canvas = new android.graphics.Canvas(bitmap);
+		canvas.drawText(topText, 50, 50, paint);
+		canvas.drawText(bottomText, 50, bitmap.getHeight() - 50, paint);
+		
+		return imageSource.fromNativeSource(bitmap);
 	}
 };
