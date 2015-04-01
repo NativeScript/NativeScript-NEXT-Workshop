4d64cf40ee1f25d7214c22edf39851facb8cdef1
TJ VanToll <tj.vantoll@gmail.com>
2015-04-01 08:38:12 -0400

    Getting social sharing to work on Android :) ^_^

--- app/app/components/social-share/social-share.android.js
+++ app/app/components/social-share/social-share.android.js
@@ -1,14 +1,23 @@
 var application = require("application");
+var context = application.android.context;
+var numberOfImagesCreated = 0;
 
 module.exports = {
 	share: function(image) {
-		var message = "Text I want to share."
-		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
-		intent.setType("text/plain");
-		intent.putExtra(android.content.Intent.EXTRA_TEXT, message);
+		numberOfImagesCreated ++;
 
-		var context = application.android.context;
-		var shareIntent = android.content.Intent.createChooser(intent, "How would you like to share this image?");
+		var intent = new android.content.Intent(android.content.Intent.ACTION_SEND);
+		intent.setType("image/jpeg");
+		
+		var stream = new java.io.ByteArrayOutputStream();
+		image.android.compress(android.graphics.Bitmap.CompressFormat.JPEG, 100, stream);
+		var path = android.provider.MediaStore.Images.Media.insertImage(
+			context.getContentResolver(), image.android, "TempMeme" + numberOfImagesCreated, null);
+		intent.putExtra(android.content.Intent.EXTRA_STREAM,
+			android.net.Uri.parse(path));
+
+		var shareIntent = android.content.Intent.createChooser(intent,
+			"How would you like to share this image?");
 		shareIntent.setFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(shareIntent);
 	}
