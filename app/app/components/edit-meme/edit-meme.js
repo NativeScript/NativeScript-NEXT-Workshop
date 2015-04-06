80cfd437ad8ef60c07c81cac1ed96ff52a9fe9cd
Clark Sell <clark.sell@csell.net>
2015-04-06 12:34:26 -0500

    added save function for a recent meme

--- app/app/components/edit-meme/edit-meme.js
+++ app/app/components/edit-meme/edit-meme.js
@@ -4,11 +4,15 @@ var observableModule = require("data/observable");
 var socialShare = require("../social-share/social-share");
 var templates = require("../templates/templates");
 
+var imageSourceModule = require("image-source");
+var fs = require("file-system");
+
 var data = new observableModule.Observable();
 var templateIndex;
 
 var _page;
 var _origImageSource;
+var _uniqueImageNameForSession;
 
 exports.loaded = function(args) {
 	_page = args.object;
@@ -42,6 +46,8 @@ exports.navigatedTo = function(args) {
 		data.set("imageSource", null);
 		invokeCamera();
 	}
+
+	_uniqueImageNameForSession = generateUUID() + ".png";
 };
 
 exports.save = function() {
@@ -51,9 +57,42 @@ exports.save = function() {
 		data.get("bottomText"),
 		data.get("isBlackText")
 	);
+
 	data.set("imageSource", image);
+
+	//Save locally
+	saveImageLocally(image, _uniqueImageNameForSession);
 };
 
 exports.share = function() {
 	socialShare.share(data.get("imageSource"));
+};
+
+exports.deleteImage = function () {
+
+};
+
+function saveImageLocally(memeImageSource, imageName) {
+
+	var documents = fs.knownFolders.documents();
+	var recentMemeFolder = documents.getFolder("recentMemes");
+	var fullPath = fs.path.join(documents.path, "recentMemes", imageName);
+	
+	var saved = memeImageSource.saveToFile(fullPath, imageSourceModule.ImageFormat.PNG);
+	
+	if (!saved) {
+		console.log("Recent meme not saved....");
+	} else {
+		console.log("Recent Meme Saved To:", fullPath);
+	}
+}
+
+function generateUUID(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 };
