20d78376a3bfda63c8096a40fac12c444c1e49f7
Clark Sell <clark.sell@csell.net>
2015-04-09 18:45:28 -0500

    refactored the file layout in create template.

--- app/app/components/create-Template/create-template.js
+++ app/app/components/create-Template/create-template.js
@@ -24,6 +24,38 @@ exports.navigatedTo = function (args){
 	invokeCamera();
 }
 
+function invokeCamera() {
+	console.log("***** INVOKING CAMERA *****");
+
+	//TODO: bug here with the promise...
+	cameraModule.takePicture()
+		.then(function(r) {
+			console.log("***** Invoke Camera Return *****", r);
+			_viewData.set("imageSource", r);
+		}, function() {
+			console.log("***** ERROR *****", error);
+		});
+
+	//TODO... if on cancel... we should show the camera roll to choose a picture from???...
+}
+
+//Save to localStorage
+exports.saveLocally = function() {
+	saveImageLocally(_viewData.get("imageSource"), _uniqueImageNameForSession);
+
+	goHome();
+};
+
+function saveImageLocally(memeImageSource, imageName) {
+	var saved = localStorage.saveTemplateLocally(imageName, memeImageSource);
+
+	if (!saved) {
+		console.log("Recent meme not saved....");
+	} else {
+		console.log("Recent template saved.");
+	}
+};
+
 //Submit the template to everlive for everyone to use.
 exports.submitToEverlive = function() {
 	everlive.addTemplate(_uniqueImageNameForSession, _viewData.get("imageSource"))
@@ -43,38 +75,6 @@ exports.submitToEverlive = function() {
 		});
 
 	goHome();
-};
-
-//Save to localStorage
-exports.saveLocally = function() {
-	saveImageLocally(_viewData.get("imageSource"), _uniqueImageNameForSession);
-
-	goHome();
-};
-
-function invokeCamera() {
-	console.log("***** INVOKING CAMERA *****");
-
-	//TODO: bug here with the promise...
-	cameraModule.takePicture()
-		.then(function(r) {
-			console.log("***** Invoke Camera Return *****", r);
-			_viewData.set("imageSource", r);
-		}, function() {
-			console.log("***** ERROR *****", error);
-		});
-
-	//TODO... if on cancel... we should show the camera roll to choose a picture from???...
-}
-
-function saveImageLocally(memeImageSource, imageName) {
-	var saved = localStorage.saveTemplateLocally(imageName, memeImageSource);
-
-	if (!saved) {
-		console.log("Recent meme not saved....");
-	} else {
-		console.log("Recent template saved.");
-	}
 };
 
 function goHome() {
