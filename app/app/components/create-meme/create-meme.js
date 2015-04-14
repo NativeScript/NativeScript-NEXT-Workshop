4f4f5c221dfd606d89c389069c6302632eca6c76
sebawita <sebawita@gmail.com>
2015-04-14 21:22:12 +0100

    Added ViewModel to create-meme

--- app/app/components/create-meme/create-meme.js
+++ app/app/components/create-meme/create-meme.js
@@ -1,89 +1,14 @@
-var imageSourceModule = require("image-source");
-var cameraModule = require("camera");
-var dialogsModule = require("ui/dialogs");
-
-var localStorage = require( "../../shared/local-storage/local-storage");
-var utilities = require( "../../shared/utilities");
-
-var imageManipulation = require("../image-manipulation/image-manipulation");
-var socialShare = require("../social-share/social-share");
-
-
-var observableModule = require("data/observable");
-var _viewData = new observableModule.Observable();
-
-var _page,
-	_origImageSource,
-	_uniqueImageNameForSession,
-	_initialised = false;
+var viewModel = require("./view-model").viewModel;
+var _page;
 
 exports.loaded = function(args) {
 	_page = args.object;
-	_page.bindingContext = _viewData;
-
-	// run this code only once
-	if(! _initialised) {
-		_initialised = true;
-		addRefreshOnChange();
-	}
+	_page.bindingContext = viewModel;
 };
 
 exports.navigatedTo = function(args) {
-
 	//grab the image from the navigation context.
 	var selectedImageSource = _page.navigationContext;
 	
-	//Save off the original...
-	_origImageSource = selectedImageSource;
-
-	_viewData.set("topText", "");
-	_viewData.set("bottomText", "");
-	_viewData.set("fontSize", 40);
-	_viewData.set("isBlackText", false);
-	_viewData.set("imageSource", selectedImageSource);
-	
-	_uniqueImageNameForSession = utilities.generateUUID() + ".png";
-};
-
-function addRefreshOnChange() {
-	_viewData.addEventListener(observableModule.knownEvents.propertyChange, function(changes) {
-		//skip if imageSource changes
-		if(changes.propertyName === "imageSource")
-			return;
-
-		refreshMeme();
-	});
-}
-
-function refreshMeme() {
-	var image = imageManipulation.addText(
-		_origImageSource,
-		_viewData.get("topText"),
-		_viewData.get("bottomText"),
-		_viewData.get("fontSize"),
-		_viewData.get("isBlackText")
-	);
-	_viewData.set("imageSource", image);
-};
-
-//Save to localStorage
-exports.saveLocally = function() {
-	refreshMeme();
-	var saved = localStorage.saveLocally(_uniqueImageNameForSession, _viewData.get("imageSource"));
-
-	if (!saved) {
-		console.log("New meme not saved....");
-	} else {
-		var options = {
-		    title: "Meme Saved",
-		    message: "Congratulations, Meme Saved!",
-		    okButtonText: "OK"
-		};
-
-		dialogsModule.alert(options);
-	}
-};
-
-exports.share = function() {
-	socialShare.share(_viewData.get("imageSource"));
+	viewModel.prepareNewMeme(selectedImageSource);
 };
--- app/app/components/create-meme/view-model.js
+++ app/app/components/create-meme/view-model.js
@@ -1,88 +1,58 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
 var observable = require("data/observable");
 var imageManipulation = require("../image-manipulation/image-manipulation");
-var localStorage = require("../local-storage/local-storage");
+var localStorage = require( "../../shared/local-storage/local-storage");
 var socialShare = require("../social-share/social-share");
+var utilities = require( "../../shared/utilities");
+var dialogsModule = require("ui/dialogs");
 
-var CreateMemeViewModel = (function (_super) {
-    __extends(CreateMemeViewModel, _super);
-    function CreateMemeViewModel() {
-        _super.apply(this, arguments);
-        this.topText = "";
-        this.bottomText = "";
-        this.fontSize = 40;
-        this.isBlackText = false;
-    }
-    CreateMemeViewModel.prototype.prepareEditor = function (image) {
-        this.selectedImage = image;
+var viewModel = new observable.Observable();
 
-        this.set("topText", "");
-        this.set("bottomText", "");
-        this.set("fontSize", 40);
-        this.set("isBlackText", false);
-        this.set("memeImage", image);
+viewModel.prepareNewMeme = function(baseImage) {
+	this.selectedImage = baseImage;
 
-        this.refreshUniqueName();
-    };
+    this.set("topText", "");
+    this.set("bottomText", "");
+    this.set("fontSize", 40);
+    this.set("isBlackText", false);
+    this.set("memeImage", baseImage);
 
-    CreateMemeViewModel.prototype.refreshMeme = function () {
-        var image = imageManipulation.addText(this.selectedImage, this.topText, this.bottomText, this.fontSize, this.isBlackText);
+    this.uniqueImageName = utilities.generateUUID() + ".png";
+};
 
-        this.set("memeImage", image);
-    };
+viewModel.refreshMeme = function () {
+    var image = imageManipulation.addText(this.selectedImage, this.topText, this.bottomText, this.fontSize, this.isBlackText);
 
-    CreateMemeViewModel.prototype.addRefreshOnChange = function () {
-        var viewModel = this;
+    this.set("memeImage", image);
+};
 
-        this.addEventListener(observable.knownEvents.propertyChange, function (changes) {
-            //skip if memeImage changes
-            if (changes.propertyName === "memeImage")
-                return;
+viewModel.saveLocally = function () {
+	this.refreshMeme();
+	var saved = localStorage.saveLocally(this.uniqueImageName, this.memeImage);
 
-            viewModel.refreshMeme();
-        });
-    };
+	if (!saved) {
+		console.log("New meme not saved....");
+	} else {
+		var options = {
+		    title: "Meme Saved",
+		    message: "Congratulations, Meme Saved!",
+		    okButtonText: "OK"
+		};
 
-    CreateMemeViewModel.prototype.refreshUniqueName = function () {
-        this.uniqueImageName = this.generateUUID() + ".png";
-    };
+		dialogsModule.alert(options);
+	}
+};
 
-    CreateMemeViewModel.prototype.generateUUID = function () {
-        var d = new Date().getTime();
-        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
-        return uuid;
-    };
+viewModel.share = function() {
+	socialShare.share(this.memeImage);
+};
 
-    CreateMemeViewModel.prototype.saveLocally = function () {
-        this.refreshMeme();
-        var saved = localStorage.saveLocally(this.uniqueImageName, this.memeImage);
+//Add event listener to refresh the memeImage every time there is a change to the params
+viewModel.addEventListener(observable.knownEvents.propertyChange, function (changes) {
+    //skip if memeImage changes
+    if (changes.propertyName === "memeImage")
+        return;
 
-        if (!saved) {
-            console.log("Recent meme not saved....");
-        } else {
-            console.log("Recent template saved.");
-        }
-    };
+    viewModel.refreshMeme();
+});
 
-    CreateMemeViewModel.prototype.share = function () {
-        try  {
-            socialShare.share(this.memeImage);
-        } catch (e) {
-            console.log(e.message);
-        }
-    };
-    return CreateMemeViewModel;
-})(observable.Observable);
-exports.CreateMemeViewModel = CreateMemeViewModel;
-
-exports.createMemeViewModel = new CreateMemeViewModel();
-//# sourceMappingURL=view-model.js.map
+exports.viewModel = viewModel;
