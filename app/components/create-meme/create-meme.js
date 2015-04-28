283113bb740c261003d9533e1391748c6fcc6e5a
sebawita <sebawita@gmail.com>
2015-04-28 21:50:33 +0100

    Tidying up Create-Meme

--- app/components/create-meme/create-meme.js
+++ app/components/create-meme/create-meme.js
@@ -1,10 +1,11 @@
 var applicationModule = require("application");
-var viewModel = require("./view-model").viewModel;
+var createMemeViewModel = require("./view-model").viewModel;
+
 var _page;
 
 exports.loaded = function(args) {
 	_page = args.object;
-	_page.bindingContext = viewModel;
+	_page.bindingContext = createMemeViewModel;
 
 	if (applicationModule.ios) {
 		_page.ios.title = "Create New";
@@ -13,7 +14,6 @@ exports.loaded = function(args) {
 
 exports.navigatedTo = function(args) {
 	//grab the image from the navigation context.
-	var selectedImageSource = _page.navigationContext;
-	
-	viewModel.prepareNewMeme(selectedImageSource);
+	var selectedImage = _page.navigationContext;
+	createMemeViewModel.prepareNewMeme(selectedImage);
 };
