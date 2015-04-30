7c3feacb500db96f126478579c7638e53a16b419
Clark Sell <clark.sell@csell.net>
2015-04-30 09:20:12 -0500

    reworked some of the images

--- app/shared/analytics.js
+++ app/shared/analytics.js
@@ -1,7 +1,7 @@
 var NativeScriptMonitor = require('./NativeScriptMonitor').Monitor;
 var monitor = new NativeScriptMonitor({
     productId: '8a1b9cc4a8f149c88237028e773b8b9d',
-    version: '0.9'
+    version: '1.1'
 });
 
 monitor.start();
